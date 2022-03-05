051a96bee8f5d24e3b791380ca5fe47efd008e4c
DaminiWandrekar <doll143wandre@gmail.com>
2022-03-05 18:58:24 +0530

    Assessment Phase-1 Project

---
+++ Virtual Key for Your Repositories/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\doll1\\eclipse-workspace\\Virtual Key for Your Repositories\\src"; 
+	
+	public static void main(String[] args) {
+		OprationList menu = new OprationList();
+		menu.introScreen();
+		menu.mainMenu();
+	}
+}
